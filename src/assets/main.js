1cf7cc9234da2b740d837c6b079e8fd4cf72f090
Omar Díaz Hernández <omardh1xxx@gmail.com>
2022-10-22 19:17:52 -0500

    second commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,8 +1,6 @@
 const API =
-  "https://youtube-v31.p.rapidapi.com/search?channelId=UCaVPhFg-Ax873wvhbNitsrQ&part=snippet%2Cid&order=date&maxResults=10";
-
+  "https://youtube-v31.p.rapidapi.com/search?channelId=UCaVPhFg-Ax873wvhbNitsrQ&part=snippet%2Cid&order=date&maxResults=9";
 const content = null || document.getElementById("content");
-
 const options = {
   method: "GET",
   headers: {
@@ -17,33 +15,32 @@ async function fetchData(urlApi) {
   return data;
 }
 
-async () => {
+(async () => {
   try {
     const videos = await fetchData(API);
     let view = `
     ${videos.items
       .map(
-        (video) => ` 
+        (video) => `
       <div class="group relative">
-          <div
-            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-          </div>
-          <div class="mt-4 flex justify-between">
-            <h3 class="text-sm text-gray-700">
-              <span aria-hidden="true" class="absolute inset-0"></span>
-            ${video.snippet.title}
-            </h3>
-          </div>
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
     `
       )
       .slice(0, 4)
       .join("")}
-   
     `;
     content.innerHTML = view;
   } catch (error) {
     console.log(error);
   }
-};
+})();
